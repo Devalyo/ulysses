b0234604b1f20a95f191f1f267bb1879f01136dc
Devalyo <danieldevalho@gmail.com>
2024-11-21 04:19:13 -0300

    style

--- events/ready.js
+++ events/ready.js
@@ -2,9 +2,11 @@ const {Events} = require('discord.js');
 
 
 module.exports = {
+
 	name: Events.ClientReady,
 	once: true,
 	execute(client) {
 		console.log(`${client.user.tag} online 🔥🔥`);
 	},
+
 };
